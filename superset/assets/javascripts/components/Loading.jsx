c8f2fe3c2ce4bbd9f8429a067b8506ec7fedd4a2
alanna scott <alanna.scott@airbnb.com>
2017-02-15 16:43:08 -0800

    add loading spinner component

---
+++ superset/assets/javascripts/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+require('../../stylesheets/loading.css');
+
+export default function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="loading-animation" />
+    </div>
+  );
+}
